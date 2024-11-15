5e29339915e0d39a7b6583bd26a7c93e2e431f71
Wpompeo <wag_pompeo@yahoo.com.br>
2024-11-15 20:02:45 -0300

    Game seed

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -18,27 +18,38 @@ public class Game {
 	private Long id;
 	private String title;
 	
-	@Column(name = "game_yer")
+	@Column(name = "game_year")
 	private Integer year;
-	private String genre;
-	private String platform;
 	private Double score;
-	private String imgUrl;
-	private String shortDescription;
+	private String genre;
+	private String imgUrl;	
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
+	private String platforms;
+	@Column(columnDefinition = "TEXT")
+	private String shortDescription;
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	//construtor sem argumento
 	public Game() {		
 	}
 	
 	//construtor com argumentos
-	public Game(Long id, String title, Integer year, String genre, String platform, Double score, String imgUrl,
+	public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
 			String shortDescription, String longDescription) {		
 		this.id = id;
 		this.title = title;
 		this.year = year;
 		this.genre = genre;
-		this.platform = platform;
+		this.platforms = platforms;
 		this.score = score;
 		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
@@ -70,10 +81,10 @@ public class Game {
 		this.genre = genre;
 	}
 	public String getPlatform() {
-		return platform;
+		return platforms;
 	}
 	public void setPlatform(String platform) {
-		this.platform = platform;
+		this.platforms = platform;
 	}
 	public Double getScore() {
 		return score;
